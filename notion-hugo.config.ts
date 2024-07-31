9e42900d947f5c338176be6645df9f05aebb4d40
invmv <invmy@outlook.com>
2024-07-31 17:09:42 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/HUGO-diary-a94993467b69418da78fa42085e788a8',
         pages: [
             // {
             //     page_id: '<page_id>',
